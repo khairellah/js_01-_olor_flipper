55a7d0c35ad26c2e69b49fe7f8f056f1cf451f0d
khairellah <zakariakhairellah@gmail.com>
2021-09-21 21:33:00 +0200

    "Addin the code for Hex.js
    -get the element from htrml page
    - create a loop
    - create an Arrow Function with parameter to get a random number"

--- hex.js
+++ hex.js
@@ -1,1 +1,19 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+// Get Elements
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color")
+
+// add tge event
+btn.addEventListener("click", function(){
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor+= hex[getRandomNumber(hex.length)];
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+})
+
+// Arraw Function with parameter
+const getRandomNumber = (size) => Math.floor(Math.random()*size);
